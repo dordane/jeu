b62f5498b0206c60a5fbc3f8956b82ee50b4ea10
Jordan Dubu <62305461+dordane@users.noreply.github.com>
2020-08-23 15:01:40 +0200

    buitebitebite

---
+++ rapido/Assets/code/House/OneWayTriggers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayTriggers : MonoBehaviour
+{
+    public bool isUp;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetButtonDown("Crouch") && player.GetComponent<PlayerMovement>().isGrounded)
+        {
+            transform.parent.GetComponent<Collider2D>().enabled = false;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            transform.parent.GetComponent<Collider2D>().enabled = isUp;
+        }
+    }
+}
